874a14ba6295dbadc717d2803f26356118ccd2b9
colorwin <b504008585@163.com>
2021-12-26 19:45:04 +0800

    Refactor

---
+++ long-common-strings1.js
@@ -0,0 +1,31 @@
+function addOne(i, j, matrix) {
+    if (matrix[i - 1] && matrix[i - 1][j - 1]) {
+        return matrix[i - 1][j - 1] + 1;
+    }
+    return 1;
+}
+
+function getMax(i, j, matrix, a, b) {
+    if (a[j] === b[j]) {
+        return addOne(i, j, matrix)
+    } else {
+        return 0
+    }
+}
+
+function longCommonStrings (a, b) {
+    let  max = 0;
+    let matrix = []
+    for (let i = 0; i < a.length; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < b.length; j++) {
+            const newMax = getMax(i, j, matrix, a, b)
+            max = Math.max(newMax, max);
+            matrix[i][j] = newMax;
+        }
+    }
+    return max;
+}
+
+const result = longCommonStrings('abcdhish', 'abcdfish')
+console.log(result === 4, result)
